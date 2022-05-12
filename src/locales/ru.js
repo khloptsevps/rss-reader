97526ecb087a7d9e3b1fc6dfbc1470e51eb1fabc
khloptsev <p.khloptsev@gmail.com>
2022-05-12 11:37:17 +1000

    add ru dictionary

---
+++ src/locales/ru.js
@@ -0,0 +1,18 @@
+export default {
+  translation: {
+    url: {
+      errors: {
+        notValidUrl: 'Ссылка должна быть валидным URL',
+        doubleUrl: 'RSS уже существует!',
+      },
+      success: 'RSS успешно загружен!',
+    },
+    buttons: {
+      previewButton: 'Просмотр',
+      readCompletelyButton: 'Читать полностью',
+      closeButton: 'Закрыть',
+    },
+    feedsTitle: 'Фиды',
+    postsTitle: 'Посты',
+  },
+};
