c1fcd549addba273e236a871a2bc715f7c28d2e4
khloptsev <p.khloptsev@gmail.com>
2022-05-12 11:38:06 +1000

    add new func

--- src/render.js
+++ src/render.js
@@ -1,18 +1,32 @@
-import { isEmpty } from 'lodash';
-
-const renderErrors = (elements, errors) => {
+/* eslint-disable no-param-reassign */
+const renderErrors = (elements, errors, i18nInstance) => {
   const { input, feedback } = elements;
-  if (!isEmpty(errors)) {
-    feedback.classList.add('text-danger');
-    feedback.textContent = errors.rssUrlErr;
-
+  if (errors) {
     input.classList.add('is-invalid');
+
+    feedback.classList.add('text-danger');
+    feedback.textContent = i18nInstance.t(errors.rssUrlErr);
     return;
   }
-  feedback.classList.remove('text-danger');
-  feedback.classList.add('text-success');
+  // временно;
   input.classList.remove('is-invalid');
-  feedback.textContent = 'RSS успешно загружен!';
+  feedback.textContent = '';
+};
+
+export const handleProcessState = (elements, processState) => {
+  switch (processState) {
+    case 'sending':
+      elements.submitButton.disabled = true;
+      elements.input.disabled = true;
+      break;
+    case 'filling':
+      elements.submitButton.disabled = false;
+      elements.input.disabled = false;
+      break;
+
+    default:
+      break;
+  }
 };
 
 export default renderErrors;
