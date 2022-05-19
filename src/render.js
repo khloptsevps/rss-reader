8deb753bca4487575f48132e2b2eb4183b30f857
khloptsev <p.khloptsev@gmail.com>
2022-05-19 23:17:11 +1000

    refactoring render, renderPosts, renderFeerd, renderError

--- src/render.js
+++ src/render.js
@@ -1,22 +1,72 @@
 /* eslint-disable no-param-reassign */
 const renderErrors = (elements, error, i18nInstance) => {
-  const { input, feedback } = elements;
-  if (error) {
-    input.classList.add('is-invalid');
+  const { input, feedback } = elements.rssForm;
 
-    feedback.classList.add('text-danger');
-    feedback.textContent = i18nInstance.t(error);
-    return;
+  const name = error === null ? null : error.name;
+
+  switch (name) {
+    case null:
+      input.classList.remove('is-invalid');
+      feedback.classList.remove('text-danger');
+      feedback.textContent = '';
+      break;
+    case 'ValidationError':
+      input.classList.add('is-invalid');
+      feedback.classList.add('text-danger');
+      feedback.textContent = i18nInstance.t(error.message.key);
+      break;
+    case 'AxiosError':
+      feedback.classList.add('text-danger');
+      feedback.textContent = i18nInstance.t('errors.networkError');
+      break;
+    case 'ParserError':
+      console.log(error);
+      feedback.classList.add('text-danger');
+      feedback.textContent = i18nInstance.t('errors.notContainValidRss');
+      break;
+    default:
+      console.log('Unknown error:', error);
+      feedback.classList.add('text-danger');
+      feedback.textContent = i18nInstance.t('errors.somethingWentWrong');
+      break;
   }
-  // временно;
-  input.classList.remove('is-invalid');
-  feedback.textContent = '';
+};
+const buildCard = (i18nInstance, cardTitle) => {
+  const card = document.createElement('div');
+  card.classList.add('card', 'border-0');
+
+  const cardBody = document.createElement('div');
+  cardBody.classList.add('card-body', 'bg-light');
+
+  const title = document.createElement('h2');
+  title.classList.add('card-title', 'h4');
+  title.textContent = i18nInstance.t(cardTitle);
+  cardBody.replaceChildren(title);
+
+  const ulEl = document.createElement('ul');
+  ulEl.classList.add('list-group', 'border-0', 'rounded-0');
+
+  card.replaceChildren(cardBody, ulEl);
+  return card;
 };
 
-const handleProcessState = (elements, processState) => {
-  const { submitButton, input } = elements;
+const renderCards = (elements, i18nInstance) => {
+  const { feeds, posts } = elements.containers;
+  feeds.replaceChildren(buildCard(i18nInstance, 'feeds'));
+  posts.replaceChildren(buildCard(i18nInstance, 'posts'));
+};
+
+const handleProcessState = (elements, processState, i18nInstance) => {
+  const {
+    form,
+    submitButton,
+    input,
+    feedback,
+  } = elements.rssForm;
+
   switch (processState) {
     case 'sending':
+      feedback.textContent = '';
       submitButton.disabled = true;
       input.disabled = true;
       break;
@@ -24,36 +74,24 @@ const handleProcessState = (elements, processState) => {
       submitButton.disabled = false;
       input.disabled = false;
       break;
-
+    case 'loaded':
+      feedback.classList.remove('text-danger');
+      feedback.classList.add('text-success');
+      feedback.textContent = i18nInstance.t(processState);
+      renderCards(elements, i18nInstance);
+      submitButton.disabled = false;
+      input.disabled = false;
+      form.reset();
+      input.focus();
+      break;
     default:
       break;
   }
 };
 
-const buildCard = (cardName) => {
-  const card = document.createElement('div');
-  card.classList.add('card', 'border-0');
-
-  const cardBody = document.createElement('div');
-  cardBody.classList.add('card-body', 'bg-light');
-
-  const cardTitle = document.createElement('h2');
-  cardTitle.classList.add('card-title', 'h5');
-  cardTitle.textContent = cardName;
-
-  cardBody.replaceChildren(cardTitle);
-  card.replaceChildren(cardBody);
-  return card;
-};
-
-const renderFeeds = (elements, feedsArray, i18nInstance) => {
-  const container = elements.feedsContainer;
-  const card = buildCard(i18nInstance.t('feedsTitle'));
-
-  const ul = document.createElement('ul');
-  ul.classList.add('list-group');
-
-  const liElements = feedsArray.map(({ title, description }) => {
+const renderFeeds = (feeds) => {
+  const feedsList = document.querySelector('.feeds ul');
+  const liElements = feeds.map(({ title, description }) => {
     const liEl = document.createElement('li');
     liEl.classList.add('list-group-item', 'bg-light', 'border-0');
 
@@ -62,33 +100,25 @@ const renderFeeds = (elements, feedsArray, i18nInstance) => {
     h3El.textContent = title;
 
     const pEl = document.createElement('p');
-    pEl.classList.add('small', 'text-black-50', 'm-0');
+    pEl.classList.add('small', 'm-0', 'text-black-50');
     pEl.textContent = description;
 
     liEl.append(h3El, pEl);
     return liEl;
   });
-  ul.append(...liElements);
-  card.replaceChildren(ul);
-  container.replaceChildren(card);
+  feedsList.replaceChildren(...liElements);
 };
 
-const renderPosts = (elements, postsArray, i18nInstance) => {
-  const container = elements.postsContainer;
-  const card = buildCard(i18nInstance.t('postsTitle'));
-
-  const ulEl = document.createElement('ul');
-  ulEl.classList.add('list-group');
-
-  const liElements = postsArray.map(({ title, link, id }) => {
+const renderPosts = (posts) => {
+  const postsList = document.querySelector('.posts ul');
+  const liElements = posts.map(({ title, link }) => {
     const liEl = document.createElement('li');
     liEl.classList.add('list-group-item', 'bg-light', 'border-0', 'd-flex', 'align-items-start');
 
     const aEl = document.createElement('a');
-    aEl.classList.add('link-primary');
+    aEl.classList.add('link-primary', 'fw-bold');
     aEl.setAttribute('href', link);
     aEl.setAttribute('target', '_blank');
-    aEl.setAttribute('data-id', id);
 
     aEl.textContent = title;
 
@@ -96,9 +126,7 @@ const renderPosts = (elements, postsArray, i18nInstance) => {
 
     return liEl;
   });
-  ulEl.append(...liElements);
-  card.replaceChildren(ulEl);
-  container.replaceChildren(card);
+  postsList.replaceChildren(...liElements);
 };
 
 const render = (elements, i18nInstance) => (path, value) => {
@@ -107,14 +135,14 @@ const render = (elements, i18nInstance) => (path, value) => {
       renderErrors(elements, value, i18nInstance);
       break;
     case 'form.processState':
-      console.log('Текущий процесс:', value);
-      handleProcessState(elements, value);
+      // console.log('Текущий процесс:', value);
+      handleProcessState(elements, value, i18nInstance);
       break;
     case 'content.feeds':
-      renderFeeds(elements, value, i18nInstance);
+      renderFeeds(value);
       break;
     case 'content.posts':
-      renderPosts(elements, value, i18nInstance);
+      renderPosts(value);
       break;
     default:
       console.log(`Неизвестный стейт ${path}`);
