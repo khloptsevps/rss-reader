736f1e86b12ad8f66a63815ae2a4fb398e5377e6
khloptsev <p.khloptsev@gmail.com>
2022-06-30 22:00:51 +1000

    update imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './styles/style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/js/dist/modal.js';
 import app from './application.js';
 
 app();
