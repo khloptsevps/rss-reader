499a5c46f49bb71370d13c87cc19904bd18d3445
khloptsev <p.khloptsev@gmail.com>
2022-07-06 10:41:23 +1000

    update updater.js

--- src/utils/updater.js
+++ src/utils/updater.js
@@ -1,15 +1,12 @@
 import axios from 'axios';
 import { includes, uniqueId } from 'lodash';
+import buildUrl from './buildUrl.js';
 import parser from './parser.js';
 
 const updater = (state) => {
   const { feeds, posts } = state.content;
   const postsLinks = posts.map(({ link }) => link);
-  if (!feeds.length) {
-    setTimeout(updater, 5000, state);
-    return;
-  }
-  const promises = feeds.map(({ id, url }) => axios.get(new URL(`/get?disableCache=true&url=${encodeURIComponent(url)}`, 'https://allorigins.hexlet.app'))
+  const promises = feeds.map(({ id, url }) => axios.get(buildUrl(url))
     .then(({ data }) => {
       const output = parser(data);
       const { items } = output;
@@ -18,14 +15,10 @@ const updater = (state) => {
         .map((item) => Object.assign(item, { feedId: id, postId: uniqueId() }));
       return newPosts;
     })
-    .catch((e) => e));
+    .catch((e) => console.error(`Error: ${e.message}`)));
   const results = Promise.all(promises);
   results
     .then((result) => result.forEach((value) => {
-      if (value.isAxiosError) {
-        console.error(`Error: ${value.message}`);
-        return;
-      }
       posts.unshift(...value);
     }))
     .finally(() => {
