e49202225f62c1c490e80ad512923157c5dc6005
khloptsev <p.khloptsev@gmail.com>
2022-07-06 10:47:33 +1000

    update validator

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,19 +1,26 @@
+/* eslint-disable no-param-reassign */
 import * as yup from 'yup';
 
-yup.setLocale({
-  string: {
-    url: (v) => ({ key: 'errors.notValidUrl', values: v }),
-  },
-  mixed: {
-    notOneOf: (v) => ({ key: 'errors.doubleUrl', values: v }),
-    required: (v) => ({ key: 'errors.required', values: v }),
-  },
-});
-
-export default (feedsContainer, link) => {
+export const initSchema = () => {
+  yup.setLocale({
+    string: {
+      url: (v) => ({ key: 'errors.notValidUrl', values: v }),
+    },
+    mixed: {
+      notOneOf: (v) => ({ key: 'errors.doubleUrl', values: v }),
+      required: (v) => ({ key: 'errors.required', values: v }),
+    },
+  });
   const schema = yup.string()
     .required()
-    .url()
-    .notOneOf(feedsContainer.map(({ url }) => url));
+    .url();
+  return schema;
+};
+
+export default (schema, feeds, link) => {
+  // schema = yup.string()
+  //   .notOneOf(feeds.map(({ url }) => url));
+  Object.assign(schema, yup.string()
+    .notOneOf(feeds.map(({ url }) => url)));
   return schema.validate(link);
 };
